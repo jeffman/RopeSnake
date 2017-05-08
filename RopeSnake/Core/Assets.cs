70ab0ebc2eec49b237b61a3fe50b717dedd39c6d
jeffman <jefferbrecht@gmail.com>
2017-05-08 01:59:49 -0400

    Added Assets

---
+++ RopeSnake/Core/Assets.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RopeSnake.Core
+{
+    public static class Assets
+    {
+        internal const string AssetsPath = "Assets";
+
+        public static Stream Open(string name)
+            => File.OpenRead(Path.Combine(AssetsPath, name));
+
+        public static T Parse<T>(string name, Func<string, T> parser)
+        {
+            return parser(Path.Combine(AssetsPath, name));
+        }
+    }
+}
