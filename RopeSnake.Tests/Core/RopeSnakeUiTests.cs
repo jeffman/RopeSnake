0be40b0dec9ade9050ae4efe6b42ed0ef852d5f9
jeffman <jefferbrecht@gmail.com>
2017-05-08 02:40:13 -0400

    Added RopeSnakeUi

---
+++ RopeSnake.Tests/Core/RopeSnakeUiTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RopeSnake.Core;
+
+namespace RopeSnake.Tests.Core
+{
+    public class TestModule : IModule
+    {
+        public string Name => "TestModule";
+
+#pragma warning disable CS0067
+        public event ModuleProgressEventHandler Progress;
+#pragma warning restore CS0067
+
+        public virtual void Initialize()
+            => throw new NotImplementedException();
+
+        public virtual void ReadFromProject(OpenResourceDelegate openResource)
+            => throw new NotImplementedException();
+
+        public virtual void ReadFromRom(Rom rom)
+            => throw new NotImplementedException();
+
+        public virtual void WriteToProject(OpenResourceDelegate openResource)
+            => throw new NotImplementedException();
+
+        public virtual void WriteToRom(Rom rom)
+            => throw new NotImplementedException();
+    }
+
+    [TestClass]
+    public class RopeSnakeUiTests
+    {
+        [TestMethod]
+        public void LoadModule()
+        {
+            var module = RopeSnakeUi.LoadModule(typeof(TestModule));
+
+            Assert.IsNotNull(module);
+            Assert.IsInstanceOfType(module, typeof(IModule));
+            Assert.AreEqual("TestModule", module.Name);
+        }
+
+        [TestMethod]
+        public void FindModuleTypes()
+        {
+            // Add more module types here as they are defined
+            var expected = new Type[]
+            {
+                typeof(TestModule)
+            };
+
+            CollectionAssert.AreEquivalent(expected, RopeSnakeUi.ModuleTypes.ToList());
+        }
+    }
+}
---
+++ RopeSnake/Core/RopeSnakeUi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace RopeSnake.Core
+{
+    public static class RopeSnakeUi
+    {
+        internal static HashSet<Type> ModuleTypes;
+
+        static RopeSnakeUi()
+        {
+            ModuleTypes = new HashSet<Type>(FindModuleTypes());
+        }
+
+        internal static IEnumerable<IModule> LoadModules(IEnumerable<Type> moduleTypes)
+        {
+            RLog.Debug($"Loading modules...");
+            return ModuleTypes.Select(t => LoadModule(t)).ToArray();
+        }
+
+        internal static IModule LoadModule(Type moduleType)
+        {
+            if (!typeof(IModule).IsAssignableFrom(moduleType))
+                throw new InvalidOperationException("Not a valid module type");
+
+            RLog.Debug($"Loading module {moduleType.FullName}...");
+            return (IModule)Activator.CreateInstance(moduleType);
+        }
+
+        internal static Type[] FindModuleTypes()
+        {
+            RLog.Debug("Finding module types...");
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var types = assemblies
+                .SelectMany(a => a.GetExportedTypes())
+                .Where(t => typeof(IModule).IsAssignableFrom(t) && t.IsClass && !t.IsAbstract)
+                .ToArray();
+
+            RLog.Debug($"Found {types.Length} module types:");
+            foreach (var type in types)
+                RLog.Debug($"  {type.FullName}");
+
+            return types;
+        }
+    }
+}
