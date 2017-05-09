11fb0a2300257e45564bc55af3f28b927887a658
jeffman <jefferbrecht@gmail.com>
2017-05-09 02:43:18 -0400

    Implemented ResourceManager

---
+++ RopeSnake.Tests/Core/ResourceManagerTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RopeSnake.Core;
+
+namespace RopeSnake.Tests.Core
+{
+    [TestClass]
+    public class ResourceManagerTests
+    {
+        private static ResourceManager manager;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Directory.CreateDirectory("temp");
+            manager = new ResourceManager("temp");
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Directory.Delete("temp", true);
+        }
+
+        private Stream Open(string name, string extension, FileMode mode)
+            => manager.Get("test", name, extension, mode);
+
+        private void Delete(string name, string extension)
+            => manager.Delete("test", name, extension);
+
+        [TestMethod]
+        public void CreateNew()
+        {
+            string testString = "MrTenda";
+
+            using (var stream = Open("new", "txt", FileMode.CreateNew))
+            {
+                stream.WriteString(testString);
+            }
+
+            var file = new FileInfo("temp\\new.txt");
+            Assert.IsTrue(file.Exists);
+            Assert.AreEqual(testString.Length, file.Length);
+            Assert.AreEqual(testString, File.ReadAllText("temp\\new.txt"));
+        }
+
+        [TestMethod]
+        public void CreateExisting()
+        {
+            File.WriteAllText("temp\\existing.txt", "To be overwritten");
+
+            string testString = "MrTenda";
+
+            using (var stream = Open("existing", "txt", FileMode.Create))
+            {
+                stream.WriteString(testString);
+            }
+
+            var file = new FileInfo("temp\\existing.txt");
+            Assert.IsTrue(file.Exists);
+            Assert.AreEqual(testString.Length, file.Length);
+            Assert.AreEqual(testString, File.ReadAllText("temp\\existing.txt"));
+        }
+
+        [TestMethod]
+        public void CreateWithNonExistingFolder()
+        {
+            string testString = "MrTenda";
+
+            using (var stream = Open("foo\\existing", "txt", FileMode.Create))
+            {
+                stream.WriteString(testString);
+            }
+
+            var file = new FileInfo("temp\\foo\\existing.txt");
+            Assert.IsTrue(file.Exists);
+            Assert.AreEqual(testString.Length, file.Length);
+            Assert.AreEqual(testString, File.ReadAllText("temp\\foo\\existing.txt"));
+        }
+
+        [TestMethod]
+        public void DeleteExisting()
+        {
+            File.WriteAllText("temp\\existing.txt", "To be deleted");
+            Delete("existing", "txt");
+            Assert.IsFalse(File.Exists("temp\\existing.txt"));
+        }
+    }
+}
--- RopeSnake/Core/Utilities/ResourceManager.cs
+++ RopeSnake/Core/Utilities/ResourceManager.cs
@@ -1,12 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FileMapDict = System.Collections.Generic.Dictionary<string,
+    System.Collections.Generic.Dictionary<string, string>>;
 
-namespace RopeSnake.Core.Utilities
+namespace RopeSnake.Core
 {
-    public class ResourceManager
+    internal sealed class ResourceManager
     {
+        internal const string DefaultExt = "bin";
+        public string Root { get; }
+        public FileMapDict FileMap { get; set; }
+
+        public ResourceManager(string root)
+            : this(root, new FileMapDict())
+        { }
+
+        public ResourceManager(string root, FileMapDict fileMap)
+        {
+            Root = Path.GetFullPath(root);
+            FileMap = fileMap ?? throw new ArgumentException(nameof(fileMap));
+        }
+
+        public void Delete(string module, string resource, string extension = DefaultExt)
+        {
+            RLog.Debug($"Deleting resource [{module}][{resource}]");
+
+            if (!FileMap.ContainsKey(module))
+                throw new InvalidOperationException($"Module not found: {module}");
+
+            if (!FileMap[module].ContainsKey(resource))
+                throw new InvalidOperationException($"Resource not found [{module}]: {resource}");
+
+            string fullPath = Path.Combine(Root, FileMap[module][resource]);
+            var file = new FileInfo(fullPath);
+
+            if (file.Exists)
+            {
+                RLog.Debug($"File exists; deleting {file.FullName}");
+                file.Delete();
+            }
+            else
+            {
+                RLog.Debug($"File exists; skipping delete {file.FullName}");
+            }
+
+            FileMap[module].Remove(resource);
+        }
+
+        public Stream Get(string module, string resource, string extension = DefaultExt, FileMode mode = FileMode.OpenOrCreate)
+        {
+            RLog.Debug($"Getting resource [{module}][{resource}] with mode {mode}");
+
+            if (!FileMap.ContainsKey(module))
+            {
+                RLog.Debug($"Creating resource filemap for {module}");
+                FileMap[module] = new Dictionary<string, string>();
+            }
+
+            if (!FileMap[module].ContainsKey(resource))
+            {
+                string defaultFile = $"{resource}.{extension}";
+                RLog.Debug($"Creating default resource lookup for {module}: {defaultFile}");
+                FileMap[module][resource] = defaultFile;
+            }
+
+            string fullPath = Path.Combine(Root, FileMap[module][resource]);
+            var directory = new DirectoryInfo(Path.GetDirectoryName(fullPath));
+
+            switch (mode)
+            {
+                case FileMode.Append:
+                case FileMode.Create:
+                case FileMode.CreateNew:
+                case FileMode.OpenOrCreate:
+                    if (!directory.Exists)
+                    {
+                        RLog.Debug($"Creating directory {directory.FullName}");
+                        directory.Create();
+                    }
+                    break;
+            }
+
+            RLog.Debug($"Opening resource {fullPath}");
+            return File.Open(fullPath, mode);
+        }
     }
 }
