72c6f0a2282c7b2027b960c58b49caa298141891
jeffman <jefferbrecht@gmail.com>
2017-05-10 12:49:16 -0400

    Added Config

---
+++ RopeSnake/Config.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace RopeSnake.Core
+{
+    public static class Config
+    {
+        public static RopeSnakeSettings Settings { get; }
+
+        static Config()
+        {
+            try
+            {
+                Settings = ConfigurationManager.GetSection("ropesnake") as RopeSnakeSettings;
+
+                if (Settings == null)
+                    throw new Exception("Settings not found");
+            }
+            catch (Exception ex)
+            {
+                RLog.Warn("Could not load RopeSnake settings. Using defaults.", ex);
+                Settings = new RopeSnakeSettings();
+            }
+        }
+    }
+
+    public sealed class RopeSnakeSettings : ConfigurationSection
+    {
+        [ConfigurationProperty("multithreaded", DefaultValue = false, IsRequired = false)]
+        public bool Multithreaded
+        {
+            get { return (bool)this["multithreaded"]; }
+            set { this["multithreaded"] = value; }
+        }
+    }
+}
