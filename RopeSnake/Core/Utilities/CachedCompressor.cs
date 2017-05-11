47788b495489dd7ef4d39f008edfeb9fea1b4da0
jeffman <jefferbrecht@gmail.com>
2017-05-10 23:59:33 -0400

    CachedCompressor: Several bugfixes and improvements
    The whole locking design was wrong before. Also now supports a static
    global cache that can be read/written from/to a directory. Different
    caches are now keyed; so an Lz77Compressor being wrapped by a
    CachedCompressor should use the key "lz77" to access that cache, for
    example.

--- RopeSnake/Core/Utilities/CachedCompressor.cs
+++ RopeSnake/Core/Utilities/CachedCompressor.cs
@@ -7,20 +7,25 @@ using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using Cache = System.Collections.Generic.Dictionary<string, byte[]>;
 
 namespace RopeSnake.Core
 {
     public sealed class CachedCompressor : ICompressor
     {
         private static HashAlgorithm _hasher;
+        private static object _hasherLock = new object();
+        private static Dictionary<string, Cache> _globalCache;
+        private static object _cacheLock = new object();
+        private const string FileExtension = "cache";
 
-        private ICompressor _compressor;
-        public ConcurrentDictionary<string, byte[]> _cache;
-        private object _lockObj = new object();
+        internal ICompressor _compressor;
+        private Cache _cache;
 
         static CachedCompressor()
         {
             _hasher = CreateHasher();
+            _globalCache = new Dictionary<string, Cache>(StringComparer.OrdinalIgnoreCase);
         }
 
         private static HashAlgorithm CreateHasher()
@@ -28,40 +33,103 @@ namespace RopeSnake.Core
             return SHA1.Create();
         }
 
-        public CachedCompressor(ICompressor compressor)
+        public CachedCompressor(ICompressor compressor, string cacheKey)
         {
             _compressor = compressor;
-            _cache = new ConcurrentDictionary<string, byte[]>();
+
+            lock (_cacheLock)
+            {
+                if (_globalCache.ContainsKey(cacheKey))
+                {
+                    _cache = _globalCache[cacheKey];
+                }
+                else
+                {
+                    _cache = CreateBlankCache();
+                    _globalCache.Add(cacheKey, _cache);
+                }
+            }
         }
 
         public byte[] Compress(byte[] source, int offset, int length)
         {
             string hash;
-            lock (_lockObj)
+            lock (_hasherLock)
             {
                 hash = string.Join("", _hasher.ComputeHash(source, offset, length).Select(b => b.ToString("x2")));
             }
 
             byte[] compressed;
 
-            if (_cache.TryGetValue(hash, out compressed))
-                return compressed.ToArray();
+            lock (_cacheLock)
+            {
+                if (_cache.TryGetValue(hash, out compressed))
+                    return compressed.ToArray();
+            }
 
             compressed = _compressor.Compress(source, offset, length);
-            _cache.TryAdd(hash, compressed);
+
+            lock (_cacheLock)
+            {
+                _cache.Add(hash, compressed);
+            }
+
             return compressed;
         }
 
         public byte[] Decompress(byte[] source, int offset)
             => _compressor.Decompress(source, offset);
 
-        public void ReadCache(string fileName)
+        public static void ReadGlobalCache(string directory)
+        {
+            lock (_cacheLock)
+            {
+                var directoryInfo = new DirectoryInfo(directory);
+                if (!directoryInfo.Exists)
+                {
+                    RLog.Warn($"Cache directory does not exist: {directoryInfo.FullName}");
+                    return;
+                }
+
+                foreach (var cacheFile in directoryInfo.GetFiles($"*.{FileExtension}"))
+                {
+                    string cacheKey = Path.GetFileNameWithoutExtension(cacheFile.Name);
+                    var cache = ReadCache(cacheFile.FullName);
+                    MergeCache(cacheKey, cache);
+                }
+            }
+        }
+
+        public static void WriteGlobalCache(string directory)
+        {
+            lock (_cacheLock)
+            {
+                foreach (var cache in _globalCache)
+                    WriteCache(cache.Value, Path.Combine(directory, $"{cache.Key}.{FileExtension}"));
+            }
+        }
+
+        internal static void MergeCache(string key, Cache cache)
+        {
+            if (_globalCache.TryGetValue(key, out Cache existing))
+            {
+                foreach (var kv in cache)
+                    existing[kv.Key] = kv.Value;
+            }
+            else
+            {
+                _globalCache.Add(key, cache);
+            }
+        }
+
+        internal static Cache ReadCache(string fileName)
         {
             try
             {
+                var cache = CreateBlankCache();
+
                 using (var stream = File.OpenRead(fileName))
                 {
-                    _cache = new ConcurrentDictionary<string, byte[]>();
                     int count = stream.ReadInt();
 
                     for (int i = 0; i < count; i++)
@@ -70,18 +138,20 @@ namespace RopeSnake.Core
                         int length = stream.ReadInt();
                         byte[] data = stream.ReadBytes(length);
 
-                        _cache.TryAdd(hash, data);
+                        cache.Add(hash, data);
                     }
                 }
+
+                return cache;
             }
             catch (Exception ex)
             {
-                RLog.Warn($"Error when reading cache.", ex);
-                _cache = new ConcurrentDictionary<string, byte[]>();
+                RLog.Warn($"Error reading cache.", ex);
+                return CreateBlankCache(); // don't return a partially initialized cache
             }
         }
 
-        public void WriteCache(string fileName)
+        internal static void WriteCache(Cache cache, string fileName)
         {
             var fullPath = Path.GetFullPath(fileName);
             var directory = new DirectoryInfo(Path.GetDirectoryName(fullPath));
@@ -90,9 +160,9 @@ namespace RopeSnake.Core
 
             using (var stream = File.Create(fileName))
             {
-                stream.WriteInt(_cache.Count);
+                stream.WriteInt(cache.Count);
 
-                foreach (var kv in _cache)
+                foreach (var kv in cache)
                 {
                     stream.WriteString(kv.Key);
                     stream.WriteByte(0);
@@ -102,5 +172,8 @@ namespace RopeSnake.Core
                 }
             }
         }
+
+        internal static Cache CreateBlankCache()
+            => new Cache(StringComparer.OrdinalIgnoreCase);
     }
 }
