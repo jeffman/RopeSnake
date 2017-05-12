f6597372b37fbb9f68c89d9b63344d490fc16f12
jeffman <jefferbrecht@gmail.com>
2017-05-12 00:15:52 -0400

    Added Canvas

---
+++ RopeSnake.Tests/Graphics/CanvasTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RopeSnake.Graphics;
+using RopeSnake.Core;
+
+namespace RopeSnake.Tests.Graphics
+{
+    [TestClass]
+    public class CanvasTests
+    {
+        private Bitmap bitmap;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            bitmap = new Bitmap(32, 32, PixelFormat.Format32bppArgb);
+        }
+
+        [TestMethod]
+        public void SetPixel()
+        {
+            using (var canvas = new Canvas(bitmap))
+            {
+                canvas.SetPixel(0, 0, Color.Blue);
+                canvas.SetPixel(31, 0, Color.Red);
+                canvas.SetPixel(16, 16, Color.Yellow);
+            }
+
+            Assert.AreEqual(Color.Blue.ToArgb(), bitmap.GetPixel(0, 0).ToArgb());
+            Assert.AreEqual(Color.Red.ToArgb(), bitmap.GetPixel(31, 0).ToArgb());
+            Assert.AreEqual(Color.Yellow.ToArgb(), bitmap.GetPixel(16, 16).ToArgb());
+        }
+
+        [TestMethod]
+        public void GetPixel()
+        {
+            bitmap.SetPixel(0, 0, Color.Blue);
+            bitmap.SetPixel(31, 0, Color.Red);
+            bitmap.SetPixel(16, 16, Color.Yellow);
+
+            using (var canvas = new Canvas(bitmap))
+            {
+                Assert.AreEqual(Color.Blue.ToArgb(), canvas.GetPixel(0, 0).ToArgb());
+                Assert.AreEqual(Color.Red.ToArgb(), canvas.GetPixel(31, 0).ToArgb());
+                Assert.AreEqual(Color.Yellow.ToArgb(), canvas.GetPixel(16, 16).ToArgb());
+            }
+        }
+
+        [TestMethod]
+        public void GetPixelOutOfBounds()
+        {
+            using (var canvas = new Canvas(bitmap))
+            {
+                Assert.ThrowsException<InvalidOperationException>(
+                    () => canvas.GetPixel(-1, -2), "Coordinate (-1, -2) out of bounds");
+
+                Assert.ThrowsException<InvalidOperationException>(
+                    () => canvas.GetPixel(32, 32), "Coordinate (32, 32) out of bounds");
+            }
+        }
+    }
+}
---
+++ RopeSnake/Graphics/Canvas.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace RopeSnake.Graphics
+{
+    public sealed class Canvas : IDisposable
+    {
+        internal Bitmap _bitmap;
+        internal BitmapData _bitmapData = null;
+        private bool _disposed;
+
+        public Canvas(Bitmap bitmap)
+        {
+            _bitmap = bitmap ?? throw new ArgumentNullException(nameof(bitmap));
+
+            if (bitmap.PixelFormat != PixelFormat.Format32bppArgb)
+                throw new InvalidOperationException("Pixel format not supported");
+
+            BeginEdit();
+        }
+
+        internal void BeginEdit()
+        {
+            _bitmapData = _bitmap.LockBits(
+                new Rectangle(0, 0, _bitmap.Width, _bitmap.Height),
+                ImageLockMode.ReadWrite,
+                _bitmap.PixelFormat);
+        }
+
+        internal void EndEdit()
+        {
+            _bitmap.UnlockBits(_bitmapData);
+        }
+
+        public unsafe void SetPixel(int x, int y, Color color)
+            => SetPixel(x, y, color.ToArgb());
+
+        public unsafe void SetPixel(int x, int y, int color)
+        {
+            if (x < 0 || x >= _bitmap.Width || y < 0 || y >= _bitmap.Height)
+                return;
+
+            int* pixels = (int*)_bitmapData.Scan0 + x + (y * _bitmapData.Stride / 4);
+            *pixels = color;
+        }
+
+        public unsafe Color GetPixel(int x, int y)
+        {
+            if (x < 0 || x >= _bitmap.Width || y < 0 || y >= _bitmap.Height)
+                throw new InvalidOperationException($"Coordinate ({x}, {y}) out of bounds");
+
+            int* pixels = (int*)_bitmapData.Scan0 + x + (y * _bitmapData.Stride / 4);
+            return Color.FromArgb(*pixels);
+        }
+
+        #region IDisposable
+
+        ~Canvas()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        internal void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                EndEdit();
+                _disposed = true;
+            }
+        }
+
+        #endregion
+    }
+}
