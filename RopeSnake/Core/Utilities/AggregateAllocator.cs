4b327ec744105b4e00123f98a5bb1023d967d90d
jeffman <jefferbrecht@gmail.com>
2017-05-16 21:17:54 -0400

    Added AggregateAllocator

---
+++ RopeSnake/Core/Utilities/AggregateAllocator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RopeSnake.Core
+{
+    public sealed class AggregateAllocator
+    {
+        public Dictionary<IModule, AllocationResult> Allocate(IDictionary<IModule, CompileResult> compileResults,
+            IAllocatable allocator)
+        {
+            var allocationResults = new Dictionary<IModule, AllocationResult>();
+
+            // Strategy: allocate largest blocks first
+            var allocateQuery = compileResults
+                .SelectMany(kv => kv.Value.AllocateBlocks.Select(b =>
+                    new
+                    {
+                        Module = kv.Key,
+                        KeyBlock = b,
+                        Alignment = kv.Value.Alignment
+                    }))
+                .OrderByDescending(c => c.KeyBlock.Value.Length)
+                .Select(c =>
+                    new
+                    {
+                        Module = c.Module,
+                        KeyBlock = c.KeyBlock,
+                        Address = allocator.Allocate(c.KeyBlock.Value.Length, c.Alignment)
+                    });
+
+            foreach (var queryResultGroup in allocateQuery.GroupBy(c => c.Module))
+            {
+                var allocationResult = new AllocationResult(queryResultGroup
+                    .ToDictionary(kv => kv.KeyBlock.Key, kv => kv.Address));
+
+                allocationResults.Add(queryResultGroup.Key, allocationResult);
+            }
+
+            return allocationResults;
+        }
+    }
+}
