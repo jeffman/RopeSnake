a14e4e816d7d50ccc1dee9ced2199e72dcb23bdc
jeffman <jefferbrecht@gmail.com>
2017-05-17 00:55:14 -0400

    Added AsmPointer

---
+++ RopeSnake.Tests/Core/JsonTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RopeSnake.Core;
+using Newtonsoft.Json;
+
+namespace RopeSnake.Tests.Core
+{
+    [TestClass]
+    public class JsonTests
+    {
+        [TestMethod]
+        public void AsmPointerRead()
+        {
+            string json = "{\"Location\":1234,\"TargetOffset\":56}";
+            AsmPointer pointer = JsonConvert.DeserializeObject<AsmPointer>(json);
+
+            Assert.AreEqual(1234, pointer.Location);
+            Assert.AreEqual(56, pointer.TargetOffset);
+        }
+
+        [TestMethod]
+        public void AsmPointerReadNoOffset()
+        {
+            string json = "1234";
+            AsmPointer pointer = JsonConvert.DeserializeObject<AsmPointer>(json);
+
+            Assert.AreEqual(1234, pointer.Location);
+            Assert.AreEqual(0, pointer.TargetOffset);
+        }
+
+        [TestMethod]
+        public void AsmPointerWrite()
+        {
+            string json = JsonConvert.SerializeObject(new AsmPointer(1234, 56));
+            Assert.AreEqual("{\"Location\":1234,\"TargetOffset\":56}", json);
+        }
+
+        [TestMethod]
+        public void AsmPointerWriteNoOffset()
+        {
+            string json = JsonConvert.SerializeObject(new AsmPointer(1234, 0));
+            Assert.AreEqual("1234", json);
+        }
+    }
+}
---
+++ RopeSnake/Core/Models/AsmPointer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RopeSnake.Core
+{
+    [JsonConverter(typeof(AsmPointerJsonConverter))]
+    public struct AsmPointer
+    {
+        public int Location { get; private set; }
+        public int TargetOffset { get; private set; }
+
+        public AsmPointer(int location, int targetOffset)
+        {
+            Location = location;
+            TargetOffset = targetOffset;
+        }
+    }
+
+    internal sealed class AsmPointerJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+            => objectType == typeof(AsmPointer);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Integer)
+            {
+                int location = serializer.Deserialize<int>(reader);
+                return new AsmPointer(location, 0);
+            }
+            else if (reader.TokenType == JsonToken.StartObject)
+            {
+                // James Newton-King, if you're reading this, we would all really
+                // appreciate a way to fall back to default serialization...
+                var obj = JToken.ReadFrom(reader);
+                return new AsmPointer((int)obj["Location"], (int)obj["TargetOffset"]);
+            }
+
+            throw new JsonException($"Unexpected token when reading AsmPointer: {reader.Path}");
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is AsmPointer pointer)
+            {
+                if (pointer.TargetOffset == 0)
+                    serializer.Serialize(writer, pointer.Location);
+                else
+                {
+                    // I'll buy you beer if you do it
+                    var obj = JObject.FromObject(new
+                    {
+                        Location = pointer.Location,
+                        TargetOffset = pointer.TargetOffset
+                    });
+                    serializer.Serialize(writer, obj);
+                }
+            }
+            else
+            {
+                throw new JsonException($"Unexpected object type when writing AsmPointer: {value.GetType()}");
+            }
+        }
+    }
+}
