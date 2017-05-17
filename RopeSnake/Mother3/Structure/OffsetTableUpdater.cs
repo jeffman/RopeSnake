162a024f29ede863e9732d2a000868dcbe89187e
jeffman <jefferbrecht@gmail.com>
2017-05-16 21:16:54 -0400

    Added Mother3.OffsetTableUpdater

---
+++ RopeSnake/Mother3/Structure/OffsetTableUpdater.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RopeSnake.Core;
+
+namespace RopeSnake.Mother3
+{
+    public sealed class OffsetTableUpdater : OffsetTableBase, ITableUpdater<TableEntry>
+    {
+        public OffsetTableUpdater(Block source, int tableOffset)
+            : base(source, tableOffset)
+        { }
+
+        public void UpdateEntry(int index, TableEntry entry)
+        {
+            if (index < 0 || index > Count)
+                throw new IndexOutOfRangeException();
+
+            int tableAddress = TableOffset + 4 + (index * 4);
+
+            if (entry == null)
+                Source.WriteInt(0, tableAddress);
+            else
+                Source.WriteInt(entry.Address - TableOffset, tableAddress);
+        }
+    }
+}
