3399113ee74e6e82c4ac1164fa9362869f1fa08b
jeffman <jefferbrecht@gmail.com>
2017-07-14 12:05:40 -0400

    Started some text helper methods

---
+++ RopeSnake/Mother3/Mother3Extensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using RopeSnake.Core;
+using RopeSnake.Mother3.Text;
+
+namespace RopeSnake.Mother3
+{
+    public static class Mother3Extensions
+    {
+        public static FixedStringTable ReadFixedStringTable(this Stream stream, Mother3TextReader reader)
+        {
+            int stringLength = stream.ReadUShort();
+            int count = stream.ReadUShort();
+
+            var table = new FixedStringTable { StringLength = stringLength };
+            reader.BaseStream.Position = stream.Position;
+
+            for (int i = 0; i < count; i++)
+            {
+                table.Strings.Add(reader.ReadString(stringLength * 2));
+            }
+
+            return table;
+        }
+
+        public static void WriteFixedStringTable(this Stream stream, FixedStringTable table, Mother3TextWriter writer)
+        {
+            stream.WriteUShort((ushort)table.StringLength);
+            stream.WriteUShort((ushort)table.Strings.Count);
+            writer.BaseStream.Position = stream.Position;
+
+            foreach (string str in table.Strings)
+                writer.WriteString(str, table.StringLength * 2);
+        }
+
+        public static List<string> ReadStringTable(this Stream stream, Mother3TextReader reader,
+            int stringsPointer, bool longOffsets = false)
+        {
+            var strings = new List<string>();
+            var offsets = new List<int>();
+
+            ushort offset;
+            while ((offset = stream.ReadUShort()) != 0xFFFF)
+            {
+                if (longOffsets)
+                    offset *= 2;
+
+                offsets.Add(offset + stringsPointer);
+            }
+
+            foreach (var stringOffset in offsets)
+            {
+                reader.BaseStream.Position = stringOffset;
+                strings.Add(reader.ReadString());
+            }
+
+            return strings;
+        }
+    }
+}
---
+++ RopeSnake/Mother3/Mother3Helpers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using RopeSnake.Core;
+using RopeSnake.Mother3.Text;
+
+namespace RopeSnake.Mother3
+{
+    internal static class Mother3Helpers
+    {
+        internal const string TextTableKey = "Text.TextTable";
+
+        public static FixedStringTable ReadFixedStringTable(Rom rom, int pointer)
+        {
+            var reader = Mother3TextReader.Create(rom, false, false);
+            var stream = rom.ToStream(pointer);
+            return stream.ReadFixedStringTable(reader);
+        }
+
+        public static void WriteFixedStringTable(FixedStringTable table, Rom rom, int pointer)
+        {
+            var writer = Mother3TextWriter.Create(rom, false, false);
+            rom.ToStream(pointer).WriteFixedStringTable(table, writer);
+        }
+
+        public static FixedStringTable ReadFixedStringTableFromTextTable(Rom rom, int tableIndex)
+        {
+            var accessor = new OffsetTableAccessor(rom, Mother3Config.Configs[rom.Type].GetAsmPointer(TextTableKey, rom));
+            return ReadFixedStringTable(rom, accessor.GetEntry(tableIndex).Address);
+        }
+
+        public static List<string> ReadStringTable(Rom rom, int offsetsPointer, int stringsPointer,
+            bool isCompressed, bool isEncoded, bool longOffsets)
+        {
+            var reader = Mother3TextReader.Create(rom, isCompressed, isEncoded);
+            var stream = rom.ToStream(offsetsPointer);
+            return stream.ReadStringTable(reader, stringsPointer, longOffsets);
+        }
+
+        public static List<string> ReadStringTableFromTextTable(Rom rom, int tableIndex)
+        {
+            var textTable = new OffsetTableAccessor(rom, Mother3Config.Configs[rom.Type].GetAsmPointer(TextTableKey, rom));
+            return ReadStringTable(rom, textTable.GetEntry(tableIndex).Address, textTable.GetEntry(tableIndex + 1).Address, false, false, false);
+        }
+    }
+}
