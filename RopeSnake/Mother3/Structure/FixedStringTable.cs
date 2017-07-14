85910ca1fba468cb0f7934ce7474854b89e17bf8
jeffman <jefferbrecht@gmail.com>
2017-07-14 12:04:16 -0400

    Added FixedStringTable

---
+++ RopeSnake/Mother3/Structure/FixedStringTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RopeSnake.Mother3
+{
+    public sealed class FixedStringTable
+    {
+        public int StringLength { get; set; }
+        public List<string> Strings { get; set; } = new List<string>();
+    }
+}
